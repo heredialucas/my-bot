314346adb385cf38e7e5486e780fdf4b30ffbf5d
Lucas Heredia <heredialucasfac22@gmail.com>
2025-04-10 15:35:16 +0200

    try with posthog config

--- packages/analytics/posthog/server.ts
+++ packages/analytics/posthog/server.ts
@@ -4,12 +4,12 @@ import { keys } from '../keys';
 
 const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = keys();
 
-if (!NEXT_PUBLIC_POSTHOG_KEY || !NEXT_PUBLIC_POSTHOG_HOST) {
-  throw new Error('PostHog configuration is missing');
-}
+// Usar valores predeterminados para entorno de desarrollo/build
+const posthogKey = NEXT_PUBLIC_POSTHOG_KEY || 'phc_dummy_key_for_build';
+const posthogHost = NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com';
 
-export const analytics = new PostHog(NEXT_PUBLIC_POSTHOG_KEY, {
-  host: NEXT_PUBLIC_POSTHOG_HOST,
+export const analytics = new PostHog(posthogKey, {
+  host: posthogHost,
 
   // Don't batch events and flush immediately - we're running in a serverless environment
   flushAt: 1,
